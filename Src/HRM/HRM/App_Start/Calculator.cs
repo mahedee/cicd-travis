aa6d72560d10105bff121ece6e3d7ea53595b80b
mahedee <mahedee.hasan@gmail.com>
2019-04-18 15:36:33 +0600

    added test project

---
+++ Src/HRM/HRM/App_Start/Calculator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HRM.App_Start
+{
+    public class Calculator
+    {
+        public double Addition(double a, double b)
+        {
+            return a + b;
+        }
+
+        public double Subtraction(double a, double b)
+        {
+            return a - b;
+        }
+
+        public double Mul(double a, double b)
+        {
+            return a * b;
+        }
+
+        public double Div(double a, double b)
+        {
+            return a / b;
+        }
+    }
+}
---
+++ Src/HRM/HRMUnitTests/CalculatorTest.cs
@@ -0,0 +1,20 @@
+﻿using HRM.App_Start;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace HRMUnitTests
+{
+    public class CalculatorTest
+    {
+        [Fact]
+        public void TestAdd()
+        {
+            Calculator calculator = new Calculator();
+            Assert.Equal(4.0, calculator.Addition(2,2));
+        }
+    }
+}
